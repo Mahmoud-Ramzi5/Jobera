b7767bd6530b4b312b9d3a29eed8728d42c739d7
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-03-26 00:00:26 +0300

    theme with local storage

--- FrontEnd/Web/src/App.jsx
+++ FrontEnd/Web/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react'
+import { useEffect, useState, useRef, createContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactSwitch from 'react-switch';
 import Register from './components/Auth/Register.jsx'
@@ -13,16 +13,36 @@ import ForgetPassword from './components/Auth/ForgetPassword.jsx';
 export const ThemeContext = createContext('');
 
 function App() {
-  const [theme, setTheme] = useState('light');
-  document.body.className = theme;
+  const initialized = useRef(false);
+  const [theme, setTheme] = useState('theme-light');
 
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
+    setTheme((currentTheme) => (currentTheme === 'theme-light' ? 'theme-dark' : 'theme-light'));
   };
+
+  useEffect(() => {
+    if (!initialized.current) {
+      initialized.current = true;
+      // Get data from local storage
+      const localTheme = localStorage.getItem('Theme');
+      if (localTheme !== null) {
+        setTheme(localTheme);
+      }
+      else {
+        setTheme('theme-light')
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('Theme', theme);
+    document.body.className = theme;
+  }, [theme]);
+
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} checkedIcon={<>🌙</>} uncheckedIcon={<>🔆</>} onColor="#4F6E95" /> 
+      <ReactSwitch onChange={toggleTheme} checked={theme === 'theme-dark'} checkedIcon={<>🌙</>} uncheckedIcon={<>🔆</>} onColor="#4F6E95" /> 
       <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
