07cb7bd525400acf4c5231b60e5e60db5818cb4e
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-03-26 00:05:54 +0300

    api fixes

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -10,17 +10,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
+Route::prefix('auth')->group(function () {
+    Route::controller(SocialAuthController::class)->group(function () {
+        Route::get('/providers', 'redirectToProviders');
+        Route::get('/{provider}/call-back', 'handleProviderCallback');
+    });
+});
 
-Route::get('/auth/providers', [SocialAuthController::class,'redirectToProviders']);
-Route::get('/auth/{provider}/call-back', [SocialAuthController::class,'handleProviderCallback']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-Route::get('/email',function(Request $request){
+Route::get('/email',function() {
     return response()->json([
-        "email"=>$request->user()->email
+        "email" => auth()->user()->email
         ]
     );
 })->middleware('auth:api');
-Route::get('/verifyEmail',[AuthController::class,'verifyEmail']);
 Route::post('/password/reset-link', [ForgetPasswordController::class, 'forgotPassword']);
 Route::post('/password/reset', [ForgetPasswordController::class, 'reset']);
--- FrontEnd/Web/src/components/Auth/NewPassword.jsx
+++ FrontEnd/Web/src/components/Auth/NewPassword.jsx
@@ -1,16 +1,43 @@
 import { useEffect, useState, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/JoberaLogo.png'
 import styles from './NewPassword.module.css'
 
-    const NewPassword = () => {
+const NewPassword = () => {
     // Define states
     const initialized = useRef(false);
     const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [confirm_password, setconfirm_Password] = useState('');
+    const [confirm_password, setConfirmPassword] = useState('');
 
     useEffect(() => {
+        if (!initialized.current) {
+            initialized.current = true;
+            // Api Call
+            fetch('http://127.0.0.1:8000/api/email', {
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                    'Accept': "application/json",
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Authorization': 'Bearer ' + searchParams.get("token")
+                }
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw new Error('Something went wrong!');
+                })
+                .then((data) => {
+                    setEmail(data.email)
+                })
+                .catch(error => {
+                    // Handle errors
+                    console.log(error);
+                });
+        }
     }, []);
 
     // Handle form submit
@@ -22,85 +49,93 @@ import styles from './NewPassword.module.css'
         event.preventDefault();
 
         // Perform Login logic (Call api)
-        fetch("http://127.0.0.1:8000/api/NewPassword", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Accept': "application/json",
-            'connection': 'keep-alive',
-            'Accept-Encoding': 'gzip, deflate, br'
-        },
-        body: JSON.stringify(
-            {
-            "email":email,
-            "password": password,
-            "confirm_password":confirm_password
+        fetch("http://127.0.0.1:8000/api/password/reset", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': "application/json",
+                'connection': 'keep-alive',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Authorization': 'Bearer ' + searchParams.get("token")
+            },
+            body: JSON.stringify(
+                {
+                    "email": email,
+                    "password": password,
+                    "confirm_password": confirm_password
+                })
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+                else {
+                    return response.json();
+                }
             })
-        })
-        .then((response) => {
-            if (!response.ok) {
-            throw new Error(response.status);
-            }
-            else {
-            return response.json();
-            }
-        })
-        .then((data) => {
-            // Do somthing with the token return from Server data['token'] 
-            console.log(data)
+            .then((data) => {
+                // Do somthing with the token return from Server data['token'] 
+                console.log(data)
 
-            // Redirect to dashboard
-            navigate('/');
-        })
-        .catch(error => {
-            // Handle errors
-            console.log(error);
-        });
+                // Redirect to dashboard
+                navigate('/');
+            })
+            .catch(error => {
+                // Handle errors
+                console.log(error);
+            });
 
         // Reset the form fields
         setPassword('');
-        setconfirm_Password('');
+        setConfirmPassword('');
     };
 
     return (
         <div className={styles.container}>
-        <div className={styles.screen}>
-            <div className={styles.screen__content}>
-            <img src={Logo} className={styles.logo} alt="logo" />
-            <form className={styles.login} onSubmit={handleSubmit}>
-                <div className={styles.login__field}>
-                <i className={`${styles.login__icon} fas fa-lock`}></i>
-                <input
-                    type="password"
-                    className={styles.login__input}
-                    placeholder="Password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <input
-                    type="password"
-                    className={styles.login__input2}
-                    placeholder="confirm password"
-                    value={confirm_password}
-                    onChange={(event) => setconfirm_Password(event.target.value)}
-                />
-                </div>
+            <div className={styles.screen}>
+                <div className={styles.screen__content}>
+                    <img src={Logo} className={styles.logo} alt="logo" />
+                    <form className={styles.login} onSubmit={handleSubmit}>
+                        <div className={styles.login__field}>
+                            <i className={`${styles.login__icon} fas fa-user`}></i>
+                            <input type="email" className={styles.login__input} value={email} readOnly={true} />
+                        </div>
+                        <div className={styles.login__field}>
+                            <i className={`${styles.login__icon} fas fa-lock`}></i>
+                            <input
+                                type="password"
+                                className={styles.login__input}
+                                placeholder="Password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                        </div>
+                        <div className={styles.login__field}>
+                            <i className={`${styles.login__icon} fas fa-lock`}></i>
+                            <input
+                                type="password"
+                                className={styles.login__input}
+                                placeholder="confirm password"
+                                value={confirm_password}
+                                onChange={(event) => setConfirmPassword(event.target.value)}
+                            />
+                        </div>
 
-                <button type="submit" className={styles.login__submit}>
-                <span className={styles.button__text}>Change password</span>
-                <i className={`${styles.button__icon} fas fa-chevron-right`}></i>
-                </button>
-            </form>
+                        <button type="submit" className={styles.login__submit}>
+                            <span className={styles.button__text}>Change password</span>
+                            <i className={`${styles.button__icon} fas fa-chevron-right`}></i>
+                        </button>
+                    </form>
+                </div>
+                <div className={styles.screen__background}>
+                    <span className={`${styles.screen__background__shape} ${styles.screen__background__shape4}`}></span>
+                    <span className={`${styles.screen__background__shape} ${styles.screen__background__shape3}`}></span>
+                    <span className={`${styles.screen__background__shape} ${styles.screen__background__shape2}`}></span>
+                    <span className={`${styles.screen__background__shape} ${styles.screen__background__shape1}`}></span>
+                </div>
             </div>
-            <div className={styles.screen__background}>
-            <span className={`${styles.screen__background__shape} ${styles.screen__background__shape4}`}></span>
-            <span className={`${styles.screen__background__shape} ${styles.screen__background__shape3}`}></span>
-            <span className={`${styles.screen__background__shape} ${styles.screen__background__shape2}`}></span>
-            <span className={`${styles.screen__background__shape} ${styles.screen__background__shape1}`}></span>
-            </div>
-        </div>
         </div>
     );
-    };
+};
 
-    export default NewPassword;
+export default NewPassword;
