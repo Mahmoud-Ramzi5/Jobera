0f58728a73cd7bea8849edab03477b18eb8f7424
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-03-25 23:36:54 +0300

    ThemeContext

--- FrontEnd/Web/src/App.jsx
+++ FrontEnd/Web/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactSwitch from 'react-switch';
 import Register from './components/Auth/Register.jsx'
 import Login from './components/Auth/Login.jsx'
 import CallBack from './components/Auth/CallBack.jsx'
@@ -8,18 +9,30 @@ import viteLogo from './assets/vite.svg'
 import NewPassword from './components/Auth/NewPassword.jsx';
 import ForgetPassword from './components/Auth/ForgetPassword.jsx';
 
+
+export const ThemeContext = createContext('');
+
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/auth/:provider/call-back" element={<CallBack />} />
-        <Route path="/NewPassword" element={<NewPassword />} />
-        <Route path="/ForgetPassword" element={<ForgetPassword />} />
-        <Route path="/" element={<></>} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/auth/:provider/call-back" element={<CallBack />} />
+          <Route path="/NewPassword" element={<NewPassword />} />
+          <Route path="/ForgetPassword" element={<ForgetPassword />} />
+          <Route path="/" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   )
 }
 
