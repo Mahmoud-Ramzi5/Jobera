f19d8689afc1d20d8b36c88705713b86886a614a
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-03-25 23:00:40 +0300

    register page CSS

--- FrontEnd/Web/src/components/Auth/Register.jsx
+++ FrontEnd/Web/src/components/Auth/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './register.module.css'
 
 const Register = () => {
   // Define states
@@ -24,46 +25,46 @@ const Register = () => {
       Clicking on a "Submit" button, prevent it from submitting a form*/
     event.preventDefault();
 
-    
+
     // Perform Login logic (Call api)
     fetch("http://127.0.0.1:8000/api/register", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Accept': "application/json",
-          'connection': 'keep-alive',
-          'Accept-Encoding': 'gzip, deflate, br'
-        },
-        body: JSON.stringify(
-            {
-                "FirstName": FirstName,
-                "LastName": LastName,
-                "email": email,
-                "password": password,
-                "ConfirmPassword": ConfirmPassword,
-                "AccountType": AccountType
-            })
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': "application/json",
+        'connection': 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br'
+      },
+      body: JSON.stringify(
+        {
+          "FirstName": FirstName,
+          "LastName": LastName,
+          "email": email,
+          "password": password,
+          "ConfirmPassword": ConfirmPassword,
+          "AccountType": AccountType
         })
-        .then((response) => {
-          if (!response.ok) { 
-            throw new Error(response.status);
-          }
-          else {
-            return response.json(); 
-          }
-        })
-        .then((data) => {
-          // Do somthing with the token return from Server data['token'] 
-          console.log(data)
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        // Do somthing with the token return from Server data['token'] 
+        console.log(data)
 
-          // Redirect to dashboard
-          navigate('/')
-        })
-        .catch(error => {
-          // Handle errors
-          console.log(error);
-        });
-    
+        // Redirect to dashboard
+        navigate('/')
+      })
+      .catch(error => {
+        // Handle errors
+        console.log(error);
+      });
+
     // Reset the form fields
     setFirstName('');
     setLastName('');
@@ -74,60 +75,104 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-        <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={FirstName}
-          onChange={(event) => setFirstName(event.target.value)}
-        />
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={LastName}
-          onChange={(event) => setLastName(event.target.value)}
-        />
+    <div className={styles.container}>
+      <div className={styles.screen}>
+        <div className={styles.screen__content}>
+          <form className={styles.register} onSubmit={handleSubmit}>
+            <div className={styles.register__row}>
+              <div className={styles.register__field}>
+                <input
+                  type="text"
+                  className={styles.register__input}
+                  placeholder="First Name"
+                  value={FirstName}
+                  onChange={(event) => setFirstName(event.target.value)}
+                />
+              </div>
+              <div className={styles.register__field}>
+                <input
+                  type="text"
+                  className={styles.register__input}
+                  placeholder="Last Name"
+                  value={LastName}
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className={styles.register__row}>
+              <div className={styles.register__field}>
+                <input
+                  type="text"
+                  className={styles.register__input}
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+              <div className={styles.register__field}>
+                <input
+                  type="date"
+                  className={styles.register__input}
+                  placeholder="Birth date" />
+              </div>
+            </div>
+            <div className={styles.register__row}>
+              <div className={styles.register__field}>
+                <input
+                  type="password"
+                  className={styles.register__input}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+              <div className={styles.register__field}>
+                <input
+                  type="password"
+                  className={styles.register__input}
+                  placeholder="Confirm Password"
+                  value={ConfirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className={styles.register__field}>
+              {accounts.map((type) => (
+                <label key={type.value}>
+                  <input
+                    type="radio"
+                    className={styles.register__input__radio}
+                    value={type.value}
+                    checked={AccountType === type.value}
+                    onChange={(event) => setAccountType(event.target.value)}
+                  />
+                  <span>{type.label}</span>
+                </label>
+              ))}
+            </div >
+            <button type="submit" className={styles.register__submit}>
+              <span className={styles.button__text}>Register now</span>
+              <i className={`${styles.button__icon} fas fa-chevron-right`}></i>
+            </button>
+          </form>
+          <div className={styles.social__register}>
+            <h3>register via</h3>
+            <div className={styles.social__icons}>
+              <a href='#' className={`${styles.social__register__icon} fab fa-google`}></a>
+              <a href='#' className={`${styles.social__register__icon} fab fa-facebook`}></a>
+              <a href='#' className={`${styles.social__register__icon} fab fa-linkedin`}></a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.screen__background}>
+          <span className={styles.screen__shape}></span>
+          <span className={`${styles.screen__background__shape} ${styles.screen__background__shape4}`}></span>
+          <span className={`${styles.screen__background__shape} ${styles.screen__background__shape3}`}></span>
+          <span className={`${styles.screen__background__shape} ${styles.screen__background__shape2}`}></span>
+          <span className={`${styles.screen__background__shape} ${styles.screen__background__shape1}`}></span>
+        </div>
       </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div>
-        <label>Confirm Password:</label>
-        <input
-          type="password"
-          value={ConfirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        />
-      </div>
-      <div>
-      {accounts.map((type) => (
-        <label key={type.value}>
-          <input
-            type="radio"
-            value={type.value}
-            checked={AccountType === type.value}
-            onChange={(event) => setAccountType(event.target.value)}
-          />
-          {type.label}
-        </label>
-      ))}
     </div>
-      <button type="submit">Login</button>
-    </form>
   );
 };
 
