4d6624d10dd6ddaabeeab358f9089cf4d2c606fd
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-03-31 14:36:02 +0300

    countries backend temp

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\State;
+use App\Models\Country;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthControllers\AuthController;
@@ -33,3 +35,38 @@ Route::controller(ForgetPasswordController::class)->group(function () {
     Route::post('/password/reset-link', 'ForgotPassword');
     Route::post('/password/reset', 'Reset')->middleware('auth:api');
 });
+
+Route::get('/GG', function () {
+    $fileContent = file_get_contents(storage_path("countries.json"));
+    $jsonContent = json_decode($fileContent, true);
+    foreach($jsonContent as $country)
+    {
+        $C = Country::create([
+            "country_id" => $country["country_id"],
+            "sortName"=> $country["sortname"],
+            "countryName"=> $country["country_name"],
+        ]);
+        foreach($country["states"] as $state) {
+            $S = State::create([
+                "state_id" => $state["state_id"],
+                "stateName"=> $state["state_name"],
+                "country_id"=> $state["country_id"],
+            ]);
+        }
+    }
+});
+
+Route::get('/countries', function () {
+    return Response()->json([
+        'C' => Country::all(),
+    ]);
+});
+
+Route::post('/states', function (Request $request) {
+    $country_id = $request->input('country_id');
+    return Response()->json([
+        'C' => State::where('country_id', $country_id)->get()->all(),
+    ]);
+});
+
+
