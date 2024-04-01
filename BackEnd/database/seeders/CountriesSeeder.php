269e182310caf6ec9f54ceb582654d01bc24e92c
ThePhenomenal-Z <alzaintab@gmail.com>
2024-04-01 17:35:59 +0300

    Countries and states

---
+++ BackEnd/database/seeders/CountriesSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\State;
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CountriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fileContent = file_get_contents(storage_path("countries.json"));
+        $jsonContent = json_decode($fileContent, true);
+        foreach($jsonContent as $country)
+        {
+            $C = Country::create([
+                "country_id" => $country["country_id"],
+                "sortName"=> $country["sortname"],
+                "countryName"=> $country["country_name"],
+            ]);
+            foreach($country["states"] as $state) {
+                $S = State::create([
+                    "state_id" => $state["state_id"],
+                    "stateName"=> $state["state_name"],
+                    "country_id"=> $state["country_id"],
+                ]);
+            }
+        }
+    }
+}
