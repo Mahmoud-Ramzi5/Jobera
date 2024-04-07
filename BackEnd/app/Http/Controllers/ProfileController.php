8e77164d452f6a349a534263e47e408558375146
ThePhenomenal-Z <alzaintab@gmail.com>
2024-04-07 18:42:41 +0300

    merge profile

--- BackEnd/app/Http/Controllers/ProfileController.php
+++ BackEnd/app/Http/Controllers/ProfileController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\skill;
 use App\Models\userSkills;
 use App\Filters\SkillFilter;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\skillResource;
 use App\Http\Resources\skillsCollection;
 use App\Http\Requests\storeSkillsRequest;
@@ -20,13 +22,12 @@ class ProfileController extends Controller
         $user = Auth::user();
         return new UserResource($user);
     }
-    public function getSkills(Request $request){
+    public function getSkills(Request $request)
+    {
         $filter = new SkillFilter();
         $queryItems = $filter->transform($request);
         if (empty($queryItems)) {
             return new skillsCollection(skill::all());
-        }
-        return new skillsCollection(skill::where($queryItems)->get());
         }
         return new skillsCollection(skill::where($queryItems)->get());
     }
