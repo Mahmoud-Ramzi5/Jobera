11511386f60ea07995d322beacd96fb37d401d80
ThePhenomenal-Z <alzaintab@gmail.com>
2024-04-09 19:02:28 +0300

    education and certificate

---
+++ BackEnd/database/migrations/2024_04_09_153859_create_education_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->enum("level",['Bachelor','Master','PHD','High School Diploma','High Institute']);
+            $table->string("feild");
+            $table->string("startDate");
+            $table->string("endDate");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('education');
+    }
+};
