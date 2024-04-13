165693695acac211e2a34df91e1bf545d5677ce1
ThePhenomenal-Z <alzaintab@gmail.com>
2024-04-13 22:30:32 +0300

    add user skills

--- FrontEnd/Web/src/apis/ProfileApis.jsx
+++ FrontEnd/Web/src/apis/ProfileApis.jsx
@@ -16,3 +16,23 @@ export const FetchUserProfile = async (token) => {
         return error.response;
     }
 };
+export const AddSkills = async (token, skillsIds) => {
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8000/api/profile/skills',
+        {
+          "skills": skillsIds.map(skillId => ({ "skill_id": skillId })),
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  };
--- FrontEnd/Web/src/pages/Register2.jsx
+++ FrontEnd/Web/src/pages/Register2.jsx
@@ -1,48 +1,47 @@
-import { useEffect, useState, useContext, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FetchSkillTypes, FetchSkills } from '../apis/AuthApis.jsx';
-import styles from '../styles/register2.module.css';
-import Inputstyles from '../styles/Input.module.css';
+import { useEffect, useState, useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { FetchSkillTypes, FetchSkills } from "../apis/AuthApis.jsx";
+import styles from "../styles/register2.module.css";
+import Inputstyles from "../styles/Input.module.css";
+import { AddSkills } from "../apis/ProfileApis.jsx";
 
 const Register2 = () => {
   const initialized = useRef(false);
   const [types, setTypes] = useState([]);
-  const [type, setType] = useState('');
+  const [type, setType] = useState("");
   const [skills, setSkills] = useState([]);
   const [skillIds, setSkillIds] = useState([]);
   const [checked, setChecked] = useState({});
   const [userSkills, setUserSkills] = useState([]);
+  const [showSubmitButton, setShowSubmitButton] = useState(false); // State for showing submit button
 
   const handleChange = (event, index) => {
-    // Destructuring
     event.persist();
     setChecked((prevState) => ({ ...prevState, [index]: true }));
-    // Case 1 : The user checks the box
-    setUserSkills((prevState) => ([
+    setUserSkills((prevState) => [
       ...prevState,
       {
         id: index,
         name: event.target.value,
       },
-    ]));
+    ]);
+    setShowSubmitButton(true); // Show submit button when a skill is chosen
   };
 
   const handleChange2 = (event, index) => {
-    // Destructuring
     event.persist();
     setChecked((prevState) => ({ ...prevState, [index]: false }));
-    console.log(index);
-    // Case 2  : The user unchecks the box
     setUserSkills((prevState) =>
       prevState.filter((skill) => skill.name !== event.target.value)
     );
+    if (userSkills.length === 1) {
+      setShowSubmitButton(false); // Hide submit button when all skills are unchosen
+    }
   };
 
   useEffect(() => {
     if (!initialized.current) {
       initialized.current = true;
-
-      // Api Call
       FetchSkillTypes().then((response) => {
         if (response.status === 200) {
           setTypes(response.data.types);
@@ -55,7 +54,6 @@ const Register2 = () => {
 
   const handleTypeSelect = (event) => {
     setType(event.target.value);
-    // Api Call
     FetchSkills(event.target.value).then((response) => {
       if (response.status === 200) {
         setSkills(response.data.skills);
@@ -69,6 +67,12 @@ const Register2 = () => {
       }
     });
   };
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    console.log(skillIds);
+    AddSkills("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOThmZWU2YjBhNWM4NjQ1MzdmMDcxYzE1NmFiYmJjODIwOWYxNTg3OTIzMzM1Mjc2OGI5Zjk0OTBkYTI2YzQ2ODJiYzFjYjg5NTliOThkMTYiLCJpYXQiOjE3MTMwMjAxMjUuMTY3NTgsIm5iZiI6MTcxMzAyMDEyNS4xNjc1ODIsImV4cCI6MTc0NDU1NjEyNS4xNTg0MjYsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.mPoISxA7AjSEFBHC1cFdAazZeY86STejWLYCX_L0BuORV05hUvtjHEiB-TXR6mU9-iAdY_sqKuGbAlw1Ep9XXjEu-ALd8n1ixag6wlo470IqCTr8iMHnOt_3YqQwS84jnhwWytCT2wWmW4fBm9JcbGcNslTspZk51cqtX_HamfotXdYjYrD8JgBMU7ui0yP-T1B0F7TAalIBwEPzADVu6U_7UZnGSRUJfkiCTygdsJxIqGLywxlrqrYUmsKwWspbEH0svj6y9wKPAedHxumSydpDo9o51YBQUHiFjHr0NvzcBrKtj6Tdzfucwi4IeVrPI0HsoymcudptApsgKRFWO7RZlefQnKXDC8Z_-h4kz5f7KFZOk60hDQwpuCTXJEIUG3fKf2TGsK5iCw8co1siJiJLouGWU61ymCrES_uNMHzmn3oaZqTULgadSD3l_PqKfO05ckpZY5d76NIlmrWFrTh2Rqb-Qa7vokfBxAnuqAADy9u2MblXVY2LNiuUwFJAgTMUuxqE0lUvPgTOxaLQ1m_UelO90VoFKXfdUb9O9zrzobiaoMQmzsolGwckcIh-LR1iwAz3HDmp0de_wGhY2oXTuBbwjWkb4Hp6wpn3oXU5FkEY7jrTfrDULpHV8UNwE8-H7qrPiER7muj8kFl67KEVggzrKOs3WxLxZL2_1eI",skillIds);
+    console.log(response.statusText);
+  }
 
   return (
     <>
@@ -132,6 +136,11 @@ const Register2 = () => {
                       ))}
                     </div>
                   </div>
+                )}
+                {showSubmitButton && (
+                  <form onSubmit={handleSubmit}>
+                    <button className={styles.submitButton}>Submit</button>
+                  </form>
                 )}
               </>
             )}
