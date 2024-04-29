21160e2d439be1dfea1e57e59f6bfaea3bccf9c7
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-04-29 15:08:32 +0300

    can't create token for companies

--- BackEnd/app/Http/Requests/CompanyRegisterRequest.php
+++ BackEnd/app/Http/Requests/CompanyRegisterRequest.php
@@ -13,7 +13,7 @@ class CompanyRegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- BackEnd/app/Models/Company.php
+++ BackEnd/app/Models/Company.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class Company extends Model
+
+class Company extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
