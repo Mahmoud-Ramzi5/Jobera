8c6f2cf17955878c7131977c2977b043765dbadc
ThePhenomenal-Z <alzaintab@gmail.com>
2024-05-04 12:07:38 +0300

    Certificates nad education register2

--- FrontEnd/Web/src/pages/Register2.jsx
+++ FrontEnd/Web/src/pages/Register2.jsx
@@ -1,34 +1,54 @@
-import { useEffect, useState, useContext, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { LoginContext } from '../App.jsx';
-import EditSkills from '../components/EditSkills.jsx';
-
-
+import { useEffect, useState, useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "../App.jsx";
+import EditSkills from "../components/EditSkills.jsx";
+import EducationForm from "../components/Profile/Education.jsx";
+import Certificates from "../components/Profile/Certificates.jsx";
 
 const Register2 = () => {
-  // Context    
-  const { loggedIn, setLoggedIn, accessToken, setAccessToken } = useContext(LoginContext);
+  // Context
+  const { loggedIn, setLoggedIn, accessToken, setAccessToken } =
+    useContext(LoginContext);
   // Define states
   const initialized = useRef(false);
   const [step, setStep] = useState("SKILLS");
   const [skills, setSkills] = useState([]);
+  const [education, setEducation] = useState({});
 
+  console.log(step);
   return (
     <>
-    {(() => {
-      switch (step) {
-        case 'SKILLS':
-          return <EditSkills edit={false} token={accessToken} register={setSkills} step={setStep}/>
-        case 'EDUCATION':
-          return <></>
-        case 'CERTIFICATES':
-          return <></>
-        case 'PORTFOLIO':
-          return <></>
-        default:
-          return <h1>Sorry, something went wrong</h1>
-      }
-    })()}
+      {(() => {
+        switch (step) {
+          case "SKILLS":
+            return (
+              <EditSkills
+                edit={false}
+                token={accessToken}
+                register={setSkills}
+                step={setStep}
+              />
+            );
+          case "EDUCATION":
+            return (
+              <EducationForm
+                edit={false}
+                token={accessToken}
+                register={setEducation}
+                step={setStep}
+              />
+            );
+
+          case "CERTIFICATES":
+            return (
+              <Certificates edit={false} token={accessToken} step={setStep} />
+            );
+          case "PORTFOLIO":
+            return <></>;
+          default:
+            return <h1>Sorry, something went wrong</h1>;
+        }
+      })()}
     </>
   );
 };
