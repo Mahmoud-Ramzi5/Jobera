9114ac1f5c407cf4768adfbc9786ba43d645edae
Ghassan-123 <ghassankk12342003@gmail.com>
2024-05-04 16:18:12 +0300

    rearranged the elements for final register2

--- FrontEnd/Web/src/pages/Register2.jsx
+++ FrontEnd/Web/src/pages/Register2.jsx
@@ -5,6 +5,8 @@ import EditSkills from "../components/EditSkills.jsx";
 import EducationForm from "../components/Profile/Education.jsx";
 import Certificates from "../components/Profile/Certificates.jsx";
 import Bar from "../components/Bar.jsx";
+import styles from "../styles/register2.module.css"
+import NavBar from "../components/NavBar.jsx";
 
 const Register2 = () => {
   // Context
@@ -19,39 +21,47 @@ const Register2 = () => {
 
 
   return (
-    <><Bar step={step}/>
-      {(() => {
-        switch (step) {
-          case "SKILLS":
-            return (
-              <EditSkills
-                edit={false}
-                token={accessToken}
-                register={setSkills}
-                step={setStep}
-              />
-            );
-          case "EDUCATION":
-            return (
-              <EducationForm
-                edit={false}
-                token={accessToken}
-                register={setEducation}
-                step={setStep}
-              />
-            );
+    <>
+    <NavBar/>
+      <div className={styles.wholescreen}>
+        <div className={styles.Bar}>
+          <Bar step={step} />
+        </div>
+        <div className={styles.steps}>
+          {(() => {
+            switch (step) {
+              case "SKILLS":
+                return (
+                  <EditSkills
+                    edit={false}
+                    token={accessToken}
+                    register={setSkills}
+                    step={setStep}
+                  />
+                );
+              case "EDUCATION":
+                return (
+                  <EducationForm
+                    edit={false}
+                    token={accessToken}
+                    register={setEducation}
+                    step={setStep}
+                  />
+                );
 
-          case "CERTIFICATES":
+              case "CERTIFICATES":
 
-            return (
-              <Certificates edit={false} token={accessToken} step={setStep} />
-            );
-          case "PORTFOLIO":
-            return <></>;
-          default:
-            return <h1>Sorry, something went wrong</h1>;
-        }
-      })()}
+                return (
+                  <Certificates edit={false} token={accessToken} step={setStep} />
+                );
+              case "PORTFOLIO":
+                return <></>;
+              default:
+                return <h1>Sorry, something went wrong</h1>;
+            }
+          })()}
+        </div>
+      </div>
     </>
   );
 };
