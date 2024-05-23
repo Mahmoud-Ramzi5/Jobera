dda4f9349a009643ca1d8f4607413e8fefdd91da
Ghassan-123 <ghassankk12342003@gmail.com>
2024-05-23 10:20:34 +0300

    changed education table name to educations

--- BackEnd/database/migrations/2024_04_09_153859_create_education_table.php
+++ BackEnd/database/migrations/2024_04_09_153859_create_education_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('education', function (Blueprint $table) {
+        Schema::create('educations', function (Blueprint $table) {
             $table->id();
             $table->enum("level", EducationLevel::names());
             $table->string("field");
