199c2f9af0b4f3a7ca0f4ff257d9d99998905bd3
Ghassan-123 <ghassankk12342003@gmail.com>
2024-06-03 11:52:39 +0300

    madde some job components but not finished

--- FrontEnd/Web/src/components/Jobs/JobCard.jsx
+++ FrontEnd/Web/src/components/Jobs/JobCard.jsx
@@ -1,5 +1,6 @@
 //import { LoginContext } from '../../utils/Contexts.jsx';
 import { useState, useContext } from 'react';
+import { Card } from 'react-bootstrap';
 import img_holder from '../../assets/upload.png';
 import styles from './jobcard.module.css';
 
@@ -7,30 +8,54 @@ const JobCard = () => {
     // Context
     //const { accessToken } = useContext(LoginContext);
     const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
     const [photo, setPhoto] = useState(null);
-    const [salary, setSalary] = useState('');
+    const [salary, setSalary] = useState(0);
     const [type, setType] = useState(null);
 
 
-
-    return (<div className={styles.container}>
-        <div className={styles.row}>
-            <div className={styles.info}>
-                <p className={styles.title}>Title: {title}</p>
-                <p className={styles.description}>Description: {description}</p>
-                <p className={styles.salary}>Salary: ${salary}</p>
-                <p className={styles.type}>type: {type}</p>
-            </div>
-            <div className={styles.imageholder}>
-                Published by: { }
-                {photo ? (
-                    <img src={URL.createObjectURL(photo)} alt="Uploaded Photo" style={{ pointerEvents: 'none' }} className={styles.image} />
-                ) : (
-                    <img src={img_holder} alt="Photo Placeholder" style={{ pointerEvents: 'none' }} className={styles.image} />
-                )}
+    // return (
+    //     <Card className={styles.cards}>
+    //         <div className={styles.background}>
+    //             <Card.Header className={styles.titles}>
+    //                 <div className={styles.title}></div>
+    //             </Card.Header>
+    //             <Card.Body>
+    //                 <Card.Title>The great job</Card.Title>
+    //                 <Card.Text>
+    //                     <p className={styles.type}>Type: {type}</p>
+    //                     <p>Salary: ${salary}</p>
+    //                     <p> Published by: { } </p>
+    //                 </Card.Text>
+    //                 <div className={styles.imageholder}>
+    //                     {photo ? (
+    //                         <img src={URL.createObjectURL(photo)} alt="Uploaded Photo" style={{ pointerEvents: 'none' }} className={styles.image} />
+    //                     ) : (
+    //                         <img src={img_holder} alt="Photo Placeholder" style={{ pointerEvents: 'none' }} className={styles.image} />
+    //                     )}
+    //                 </div>
+    //             </Card.Body>
+    //         </div>
+    //     </Card>
+    // )
+    return (
+        <div className={styles.container}>
+            <div className={styles.row}>
+                <div className={styles.info}>
+                    <h5 className={styles.title}>the great job{title}</h5>
+                    <p className={styles.type}>Type: {type}</p>
+                    <p>Salary: ${salary}</p>
+                    <p> Published by: { } </p>
+                </div>
+                <div className={styles.imageholder}>
+                    {photo ? (
+                        <img src={URL.createObjectURL(photo)} alt="Uploaded Photo" style={{ pointerEvents: 'none' }} className={styles.image} />
+                    ) : (
+                        <img src={img_holder} alt="Photo Placeholder" style={{ pointerEvents: 'none' }} className={styles.image} />
+                    )}
+                </div>
             </div>
         </div>
-    </div>);
+    );
 };
+
 export default JobCard;
---
+++ FrontEnd/Web/src/components/Jobs/ShowJob.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+import { LoginContext } from '../../utils/Contexts';
+import img_holder from '../../assets/upload.png';
+import styles from './showjob.module.css';
+
+const ShowJob = () => {
+    const { accessToken } = useContext(LoginContext);
+    const [isLoading, setIsLoading] = useState(true);
+    const [job, setJob] = useState(
+        {
+            title: "",
+            description: "",
+            photo: null,
+            salary: 0,
+            type: "",
+            skills: [{id:1,name:'ssssssss'}]
+        });
+
+    // useEffect(() => {
+    //     if (accessToken) {
+    //         setIsLoading(true);
+    //         FetchJob(accessToken,id).then((response) => {
+    //             if (response.status === 200) {
+    //                 // get the info for the job
+    //                 setJob(response.data);
+    //             }
+    //             else {
+    //                 console.log(response.statusText);
+    //             }
+    //         }).then(() => {
+    //             setIsLoading(false);
+    //         });
+    //     }
+    // });
+
+    // if (isLoading) {
+    //     return <div id='loader'><div className="clock-loader"></div></div>
+    // }
+
+    return (<div className={styles.jobsPage}>
+        <div className={styles.pagecontent}>
+            <div className={styles.leftSideContainer}>
+                <div className={styles.title}>my great project{job.title}</div>
+                <div className={styles.description}>Description: {job.description}</div>
+                <div className={styles.left}>salary: ${job.salary}</div>
+                <div className={styles.left}>type: {job.type}</div>
+                <h4 className={styles.heading}>Skills wanted:</h4>
+                <div className={styles.data}>
+                    {job.skills.map((skill) => (
+                        <div key={skill.id} className={styles.used_skills}>
+                            <div className={styles.used_skill}>{skill.name}</div>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.comments}>
+                    <h5>Comments</h5>
+                </div>
+            </div>
+            <div className={styles.rightSideContainer}>
+                <div className={styles.name}>published by: { }</div>
+                <div className={styles.imageholder}>
+                    <div className={styles.image}>
+                        {job.photo ? (
+                            <img src={URL.createObjectURL(job.photo)} alt="Uploaded Photo" style={{ pointerEvents: 'none' }} className={styles.image} />
+                        ) : (
+                            <img src={img_holder} alt="Photo Placeholder" style={{ pointerEvents: 'none' }} className={styles.image} />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default ShowJob
