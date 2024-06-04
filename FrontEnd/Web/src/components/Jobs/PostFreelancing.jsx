6b5235dcaf7d2c26b434ca95f4c8f55610da7a8a
Ghassan-123 <ghassankk12342003@gmail.com>
2024-06-04 15:07:46 +0300

    something fixed

--- FrontEnd/Web/src/components/Jobs/PostFreelancing.jsx
+++ FrontEnd/Web/src/components/Jobs/PostFreelancing.jsx
@@ -9,7 +9,6 @@ import NormalInput from '../NormalInput.jsx';
 import img_holder from '../../assets/upload.png';
 import styles from './job.module.css';
 import Inputstyles from '../../styles/Input.module.css';
-import { AddFreelancingJobAPI } from '../../apis/JobsApis.jsx';
 
 const PostFreelancing = () => {
     // Context
