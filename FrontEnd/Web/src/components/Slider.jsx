c2dd2ed1ff41f51123a912dbb55b60ff60eeb143
Mahmoud-Ramzi5 <mzramzi5@gmail.com>
2024-06-15 13:50:09 +0300

    some work in filters (not done, not working yet)

---
+++ FrontEnd/Web/src/components/Slider.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState, useContext, useRef } from 'react';
+import ReactSlider from 'react-slider';
+import './slider.css';
+
+const Slider = ({ values, handler }) => {
+
+  return (
+    <ReactSlider
+      className='react_slider'
+      thumbClassName='reactSlider_thumb'
+      trackClassName='reactSlider_track'
+      pearling
+      min={0}
+      max={100000}
+      minDistance={1}
+
+      value={values}
+      onChange={(values, index) => {
+        if (index === 0) {
+          handler((prevState) => ({ ...prevState, minSalary: values[index] }));
+        }
+        else if (index === 1) {
+          handler((prevState) => ({ ...prevState, maxSalary: values[index] }));
+        }
+        else { }
+      }}
+
+      ariaLabel={['Lower thumb', 'Upper thumb']}
+      ariaValuetext={state => `Thumb value ${state.valueNow}`}
+    />
+  );
+}
+
+export default Slider;
--- FrontEnd/Web/src/pages/Jobs/DefJobs.jsx
+++ FrontEnd/Web/src/pages/Jobs/DefJobs.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useContext, useRef } from 'react';
 import { LoginContext } from '../../utils/Contexts.jsx';
 import { FetchJobs } from '../../apis/JobsApis.jsx';
+import Slider from '../../components/Slider.jsx';
 import JobCard from '../../components/Jobs/JobCard.jsx';
 import styles from '../../styles/jobs.module.css';
 
@@ -14,6 +15,19 @@ const DefJobs = () => {
   const [isDone, setIsDone] = useState(false);
   const [nextPage, setNextPage] = useState(1);
   const DataSize = 10;
+  const types = [
+    { value: 'All', label: 'All', icon: <></> },
+    { value: 'FullTime', label: 'FullTime', icon: <></> },
+    { value: 'PartTime', label: 'PartTime', icon: <></> },
+    { value: 'Freelancinng', label: 'Freelancinng', icon: <></> },
+  ];
+
+  const [filter, setFilter] = useState({
+    user: '',
+    type: 'All',
+    minSalary: 0,
+    maxSalary: 100000
+  });
 
   const [jobs, setJobs] = useState([]);
   const [data, setData] = useState([]);
@@ -34,14 +48,20 @@ const DefJobs = () => {
             setIsDone(true);
           }
           response.data.jobs.map((job) => {
-            if (job.photo) {
-              FetchImage("", job.photo).then((response) => {
-                job.photo = response;
+
+            // Check if job is already in array
+            if (!jobs.some(item => job.id === item.id)) {
+
+              // if not add job
+              if (job.photo) {
+                FetchImage("", job.photo).then((response) => {
+                  job.photo = response;
+                  setJobs((prevState) => ([...prevState, job]));
+                });
+              }
+              else {
                 setJobs((prevState) => ([...prevState, job]));
-              });
-            }
-            else {
-              setJobs((prevState) => ([...prevState, job]));
+              }
             }
           });
         }
@@ -74,16 +94,79 @@ const DefJobs = () => {
   }, [nextPage]);
 
 
+
+  const handleFilter = (event) => {
+    const { name, value } = event.target;
+    setFilter({ ...filter, [name]: value })
+  }
+
+  console.log(filter);
+
   return (
     <div className={styles.screen}>
-      <div className={styles.container}>
+      <div className={styles.left_container}>
+        <label htmlFor='user'>
+          Published By:
+        </label>
+        <input
+          type='text'
+          placeholder='Published by'
+          id='user'
+          name='user'
+          value={filter.user}
+          onChange={handleFilter}
+
+        />
+
+
+        {types.map((T) => (
+          <div className={styles.GG} key={T.value}>
+            <input
+              type="radio"
+              name='type'
+              value={T.value}
+              checked={filter.type === T.value}
+              onChange={handleFilter}
+            />
+            <i>{T.icon}</i>
+            <label>{T.label}</label>
+          </div>
+        ))}
+
+        <div>
+          <Slider values={[filter.minSalary, filter.maxSalary]} handler={setFilter} />
+          <div className={styles.slider_area}>
+            <input
+              type='number'
+              name='minSalary'
+              min={0}
+              max={100000}
+
+              value={filter.minSalary}
+              onChange={handleFilter}
+            />
+
+            <input
+              type='number'
+              name='maxSalary'
+              min={0}
+              max={100000}
+
+              value={filter.maxSalary}
+              onChange={handleFilter}
+            />
+          </div>
+        </div>
+
+      </div>
+      <div className={styles.right_container}>
         {jobs.map((job) => (
           <JobCard key={job.defJob_id} JobData={job} />
         ))}
+        {isLoading ? <div id='loader'><div className="clock-loader"></div></div>
+          : isDone && <h5 className={styles.done}>No more jobs to show</h5>
+        }
       </div>
-      {isLoading ? <div id='loader'><div className="clock-loader"></div></div>
-        : isDone && <h5 className={styles.done}>No more jobs to show</h5>
-      }
     </div>
   );
 }
