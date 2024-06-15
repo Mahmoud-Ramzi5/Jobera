4b1b8dac1bf1599f811b3804ac4005d55129a476
Ghassan-123 <ghassankk12342003@gmail.com>
2024-06-15 14:32:56 +0300

    almost finished displayjob still need id and conditions

--- FrontEnd/Web/src/components/Jobs/ShowJob.jsx
+++ FrontEnd/Web/src/components/Jobs/ShowJob.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
+import { PencilSquare, CurrencyDollar } from 'react-bootstrap-icons';
 import { LoginContext } from '../../utils/Contexts';
-import { FetchJob } from '../../apis/JobsApis';
+import { FetchJob, ApplyToRegJobAPI, ApplyToFreelancingJobAPI } from '../../apis/JobsApis';
+import NormalInput from '../NormalInput';
 import JobCompetetorCard from './JobCompetetorCard';
 import img_holder from '../../assets/upload.png';
 import styles from './css/showjob.module.css';
+import Inputstyles from '../../styles/Input.module.css'
 
 
 const ShowJob = () => {
@@ -13,11 +16,14 @@ const ShowJob = () => {
   // Define states
   const [isLoading, setIsLoading] = useState(true);
   const [job, setJob] = useState({});
+  const [participate, setParticipate] = useState(false);
+  const [comment, setComment] = useState('');
+  const [desiredSalary, setDesiredSalary] = useState('');
 
   useEffect(() => {
     if (accessToken) {
       setIsLoading(true);
-      FetchJob(accessToken, 11).then((response) => {
+      FetchJob(accessToken, 3).then((response) => {
         console.log(response);
         if (response.status === 200) {
           // get the info for the job
@@ -36,10 +42,42 @@ const ShowJob = () => {
     return <div id='loader'><div className="clock-loader"></div></div>
   }
 
+  const handleNewCompetitor = (event) => {
+    event.preventDefault();
+    console.log('we are number one');
+    setParticipate(true);
+  }
+
+  const handleNewJobCompetitor = (event) => {
+    event.preventDefault();
+    console.log(comment);
+    ApplyToRegJobAPI(accessToken, 3, comment).then((response) => {
+      if (response.status == 200) {
+        console.log('added a competitor succsefully')
+      }
+      else {
+        console.log(response.statusText);
+      }
+    })
+  }
+
+  const handleNewFreelancer = (event) => {
+    event.preventDefault();
+    console.log(comment, desiredSalary);
+    ApplyToFreelancingJobAPI(accessToken, 3, comment, desiredSalary).then((response) => {
+      if (response.status == 200) {
+        console.log('added a competitor succsefully')
+      }
+      else {
+        console.log(response.statusText);
+      }
+    });
+  }
+
   return (
     <div className={styles.jobsPage}>
       <div className={styles.pagecontent}>
-        <div className={styles.leftSideContainer}>
+        <div className={styles.left_side_container}>
           <div className={styles.imageholder}>
             <div className={styles.image}>
               {job.individual && job.individual.avatar_photo ? (
@@ -66,7 +104,12 @@ const ShowJob = () => {
           <div className={styles.titleholder}>
             <div className={styles.title}>{job.title}</div>
           </div>
-          <div className={styles.name}>Job owner: {job.individual ? (job.individual.full_name) : (job.company.name)}</div>
+          <div className={styles.name}>Job owner: {job.job_user ?
+            (job.job_user.full_name ?
+              job.job_user.full_name
+              : job.job_user.name)
+            : (job.company.name)}
+          </div>
           <div className={styles.type}>{job.type}  job</div>
           <div className={styles.description}>Description: {job.description}</div>
           {job.salary ? (
@@ -77,7 +120,48 @@ const ShowJob = () => {
         </div>
       </div>
       <div className={styles.competetors}>
-        <h5>Competitors</h5>
+        <div className={styles.name_and_button}>
+          <h5>Competitors</h5>
+          <button className={styles.competetor_button} onClick={handleNewCompetitor}>+ Be a competitor</button>
+        </div>
+        {participate && job.type === 'Freelancing' ? (<>
+          <div className={Inputstyles.field}>
+            <i className={Inputstyles.icon}><PencilSquare /></i>
+            <textarea
+              placeholder='add your comment'
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+              className={Inputstyles.input}
+              rows='5'
+            />
+          </div>
+          <div className={styles.money_holder}>
+            <NormalInput
+              type='number'
+              placeholder='DesiredSalary'
+              icon={<CurrencyDollar />}
+              value={desiredSalary}
+              setChange={setDesiredSalary}
+            />
+            <button className={styles.send_button} onClick={handleNewFreelancer}>send</button>
+
+          </div>
+        </>
+        ) : (<></>)}
+        {participate && job.type != 'Freelancing' ? (<>
+          <div className={Inputstyles.field}>
+            <i className={Inputstyles.icon}><PencilSquare /></i>
+            <textarea
+              placeholder='add your comment'
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+              className={Inputstyles.input}
+              rows='5'
+            />
+          </div>
+          <button className={styles.send_button} onClick={handleNewJobCompetitor}>send</button>
+        </>
+        ) : (<></>)}
         {job.competetors && job.competetors.map((competetor) => (
           <JobCompetetorCard key={competetor.id} CompetetorData={competetor} />
         ))}
