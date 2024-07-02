2003fc10099b7dfb71541573ebfa96dd8f4dfa14
ThePhenomenal-Z <alzaintab@gmail.com>
2024-07-02 12:50:08 +0300

    competetor Regjob

--- BackEnd/app/Http/Requests/AcceptIndividualRequest.php
+++ BackEnd/app/Http/Requests/AcceptIndividualRequest.php
@@ -11,7 +11,7 @@ class AcceptIndividualRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class AcceptIndividualRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+           'reg_job_competetor_id'=>['required','exists:reg_job_competetors,id']
         ];
     }
 }
