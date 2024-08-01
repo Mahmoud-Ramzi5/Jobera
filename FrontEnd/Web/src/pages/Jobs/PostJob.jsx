684a308625911d042cc20b1c55ce57bd26ee9680
Ghassan-123 <ghassankk12342003@gmail.com>
2024-08-01 13:43:43 +0300

    changed post images to icons

--- FrontEnd/Web/src/pages/Jobs/PostJob.jsx
+++ FrontEnd/Web/src/pages/Jobs/PostJob.jsx
@@ -2,6 +2,8 @@ import { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Card } from 'react-bootstrap';
 import { ProfileContext } from '../../utils/Contexts';
+import { BsSuitcaseLg, BsClockHistory } from 'react-icons/bs';
+import { FaLaptopCode } from 'react-icons/fa'
 import PostRegJob from '../../components/Jobs/PostRegJob';
 import PostFreelancing from '../../components/Jobs/PostFreelancing';
 import FullTimeJob from '../../assets/fullTimeJob.png'
@@ -36,55 +38,27 @@ const PostJob = () => {
       <div className={styles.screen}>
         <div className={styles.screen_content}>
           <h2 className={styles.heading}>
-            {t('pages.post_job.heading')}
+            {t('pages.post_job.heading1')}{' '}
+            {type === 'FullTime' ? t('pages.post_job.title_fulltime')
+              : type === 'PartTime' ? t('pages.post_job.title_parttime')
+                : type === 'Freelancing' ? t('pages.post_job.title_freelancing')
+                  : ''}{' '}
+            {t('pages.post_job.heading2')}
           </h2>
           <div className={styles.all_cards}>
             {profile.type === "company" && <>
               <div className={styles.card_limit}>
-                <Card onClick={handleFullTimeJob}>
-                  <div className={styles.card_background}>
-                    <Card.Img
-                      className={styles.Card_Img}
-                      variant="top"
-                      src={FullTimeJob}
-                      alt='FullTimeJob'
-                    />
-                    <Card.Title className={styles.title}>
-                      {t('pages.post_job.title_fulltime')}
-                    </Card.Title>
-                  </div>
-                </Card>
+                <BsSuitcaseLg className={styles.job_icon} onClick={handleFullTimeJob} />
+                <h5 className={styles.title}>{t('pages.post_job.title_fulltime')}</h5>
               </div>
               <div className={styles.card_limit}>
-                <Card onClick={handlePartTimeJob}>
-                  <div className={styles.card_background}>
-                    <Card.Img
-                      className={styles.Card_Img}
-                      variant="top"
-                      src={PartTimeJob}
-                      alt='PartTimeJob'
-                    />
-                    <Card.Title className={styles.title}>
-                      {t('pages.post_job.title_parttime')}
-                    </Card.Title>
-                  </div>
-                </Card>
+                <BsClockHistory className={styles.job_icon} onClick={handlePartTimeJob} />
+                <h5 className={styles.title}>{t('pages.post_job.title_parttime')}</h5>
               </div>
             </>}
             <div className={styles.card_limit}>
-              <Card onClick={handleFreelancingJob}>
-                <div className={styles.card_background}>
-                  <Card.Img
-                    className={styles.Card_Img}
-                    variant="top"
-                    src={FreelancingJob}
-                    alt='FreelancingJob'
-                  />
-                  <Card.Title className={styles.title}>
-                    {t('pages.post_job.title_freelancing')}
-                  </Card.Title>
-                </div>
-              </Card>
+              <FaLaptopCode className={styles.job_icon} onClick={handleFreelancingJob} />
+              <h5 className={styles.title}>{t('pages.post_job.title_freelancing')}</h5>
             </div>
           </div>
           {type ?
