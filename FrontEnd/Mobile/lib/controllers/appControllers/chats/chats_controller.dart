592421cae71d70aa0fdcffbbd2ddebb3744a9f62
mohamed majzoub <mohamed13majzoub@gmail.com>
2024-08-12 20:22:10 +0300

    whatsapp clone

--- FrontEnd/Mobile/lib/controllers/appControllers/chats/chats_controller.dart
+++ FrontEnd/Mobile/lib/controllers/appControllers/chats/chats_controller.dart
@@ -139,8 +139,8 @@ class ChatsController extends GetxController {
       // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
       const AndroidInitializationSettings initializationSettingsAndroid =
           AndroidInitializationSettings('app_icon');
-      final InitializationSettings initializationSettings =
-          const InitializationSettings(android: initializationSettingsAndroid);
+      const InitializationSettings initializationSettings =
+          InitializationSettings(android: initializationSettingsAndroid);
       // await flutterLocalNotificationsPlugin.initialize(initializationSettings,
       //     onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
     }
